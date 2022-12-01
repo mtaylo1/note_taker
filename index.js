77a9ce4787cbb905c8ddd4db264fe6c9c418152f
M Taylor <107767318+mtaylo1@users.noreply.github.com>
2022-12-01 12:06:05 -0500

    testing deploy

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ const path = require('path')
 const app = express()
 const PORT = process.env.PORT || 3001
 
-app.use(express.static(path.join(__dirname, "frontend", "build")));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/api", async (req, res) => {
     res.send({ message:"Hello "});
     });
 app.get("*", async (req, res) => {
-    res.sendFile(path.join(__dirname, "frontend", "build", "index.html"));
+    res.sendFile(path.join(__dirname, "public", "notes.html", "index.html"));
 });
 
 app.listen(PORT, ()=>console.log('🌐This sever is running on '+PORT))
